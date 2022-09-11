a1468046db7b039783dbe51fa49cd93005f8fcb7
Rebecca-Nyambura <rndirangh@gmail.com>
2022-09-11 06:11:56 -0700

    9. Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible combinations of single-digit numbers
- *        only use the putchar function a maximum of four times
+ * main - Prints all possible combinations of single-digit numbers.
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 int main(void)
 {
 	int num;
 
 	for (num = 0; num <= 9; num++)
+	{
 		putchar((num % 10) + '0');
-	if (num == 9)
-		continue;
+		if (num == 9)
+			continue;
 
-	putchar(',');
-	putchar(' ');
-}
+		putchar(',');
+		putchar(' ');
+	}
 
-putchar('\n');
+	putchar('\n');
 
-return (0);
+	return (0);
 }
