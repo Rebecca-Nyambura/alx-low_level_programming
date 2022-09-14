daece1689eb5f92502a0b78417524516396984ef
Rebecca-Nyambura <rndirangh@gmail.com>
2022-09-14 11:56:26 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
-#include <unistd.h>
 #include "main.h"
 
 /**
- * main - prints _putchar followed by a new line
+ * main - prints _putchar
+ * followed by a new line
  * Return: Always 0
  */
-int main(void)
+int main (void)
 {
 	char c[] = "_putchar";
 
