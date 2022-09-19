80f9418bae2f2b1a95ba7a4a8bbff19b31b2a1b9
Rebecca-Nyambura <rndirangh@gmail.com>
2022-09-19 13:48:59 -0700

    2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - calculate length of a string
+ * @s: the string to calculate length
+ * Return: length of a string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+	int length = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		length ++;
+	}
+	return (length);
+}
