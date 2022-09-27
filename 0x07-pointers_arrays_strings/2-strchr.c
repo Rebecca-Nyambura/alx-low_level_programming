2754bd56a62939d9e4ba3a438bebc32ee8bd85d0
Rebecca-Nyambura <rndirangh@gmail.com>
2022-09-27 01:13:44 -0700

    2. strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,13 +11,16 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i;
+	while (*s)
+	{
+		s++;
 
-	for (i = 0; (s[i] != c) && (s[i] != '\0'); i++)
-		;
-	if (s[i] == c)
-		return (s + i);
-	else
-		return (NULL);
+		if (*s == c)
+		{
+			return (s);
+		}
+
+	}
+
+	return (0);
 }
-
