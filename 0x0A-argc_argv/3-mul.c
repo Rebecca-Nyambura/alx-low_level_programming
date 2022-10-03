a47534138a448d079341ea355ec2f0f4da0b4711
Rebecca-Nyambura <rndirangh@gmail.com>
2022-10-03 00:39:25 -0700

    3. Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,12 +1,12 @@
-#include "main.h"
-#include "stdio.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - program that multiplies two numbers
- * @argc: number of arguments supplied to the program
- * @argc: array of pointers to the arguments
+ * main - Prints the multiplication of two numbers, followed by a new line.
+ * @argc: The number of arguments supplied to the program
+ * @argv: An array of pointers to the arguments
  * Return: If the program receives two arguments - 0.
- * If the program does not receive two arguments - 1.
+ * If the program does not receive two arguments - 1
  */
 int main(int argc, char *argv[])
 {
