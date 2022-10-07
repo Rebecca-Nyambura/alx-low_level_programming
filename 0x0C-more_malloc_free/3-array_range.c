0d39aeeacae7a2778ac19085aa09c91dc4e07063
Rebecca-Nyambura <rndirangh@gmail.com>
2022-10-07 01:06:41 -0700

    3. array_range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,7 +15,7 @@ int *array_range(int min, int max)
 	int *block;
 	int i, j = 0;
 
-	if min > max
+	if (min > max)
 		return (NULL);
 	block = malloc(sizeof(*block) * ((max - min) + 1));
 	if (block != NULL)
