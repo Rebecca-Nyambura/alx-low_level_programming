3d0f8d783e832df1f6d2a8b84e56f57d1328ef2e
Rebecca-Nyambura <rndirangh@gmail.com>
2022-10-27 01:56:14 -0700

    12. Print (safe version)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -5,7 +5,8 @@
 
 /**
  * print_listint_safe - function that prints a listint_t linked list
- * @head: pointer to the address of the head of the listint_t linked list
+ * @head: list of type listint_t to print
+ *
  * Return: number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
