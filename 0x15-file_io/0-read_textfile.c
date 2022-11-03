247486e3691702ccb9ec7cf678411f626300cd1d
Rebecca-Nyambura <rndirangh@gmail.com>
2022-11-03 00:14:55 -0700

    0. Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,37 +1,39 @@
-#include "main.h"
-#include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to the standard output
+ * read_textfile - reads a text file and prints it to the POSIX
+ * standard output
  * @filename: name of the file to be read
- * @letters: number of letters to read and print
+ * @letters: letters to be read and printed
  * Return: the number of letters printed, or 0 if it failed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	int i, y;
+	int fd, chars_read, chars_written;
 	char *buf;
-	if (!filename)
+
+	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
-	if (fd < 0)
+	if (fd == -1)
 		return (0);
-	i = read(fd, buf, letters);
-	if (i < 0)
-	{
-		free(buf);
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
 		return (0);
-	}
-	buf[i] = '\0';
+	chars_read = read(fd, buf, letters);
+	if (chars_read == -1)
+		return (0);
+	chars_read = read(fd, buf, letters);
+	if (chars_read == -1)
+		return (0);
+	chars_written = write(STDOUT_FILENO, buf, chars_read);
+	if (chars_written == -1)
+		return (0);
 	close(fd);
-	y = write(STDOUT_FILENO, buf, i);
-	if (y < 0)
-	{
-		free(buf);
-		return (0);
-	}
 	free(buf);
-	return (y);
+	return (chars_written);
 }
