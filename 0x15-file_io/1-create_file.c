b82a9ee06535aab019274e0b505a2bb12be22b39
Rebecca-Nyambura <rndirangh@gmail.com>
2022-11-03 00:46:27 -0700

    1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,35 +1,37 @@
 #include "main.h"
 
 /**
- * read_textfile - reads text file and print it to standard output
- * @filename: the name of the file
- * @letters: number of letters to be read and printed
- * Return: number of letters printed, 0 if it fails
+ * create_file - function that creates a file
+ * @filename: name of file to create
+ * @text_content: NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure
  */
-ssize_t read_textfile(const char *filename, size_t letters)
+int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t nrd, nwr;
-	char *buf;
+	int nletters;
+	int rwr;
 
 	if (!filename)
-		return (0);
+		return (-1);
 
-	fd = open(filename, O_RDONLY);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
-		return (0);
+		return (-1);
 
-	buf = malloc(sizeof(char) * (letters));
-	if (!buf)
-		return (0);
+	if (!text_content)
+		text_content = "";
 
-	nrd = read(fd, buf, letters);
-	nwr = write(STDOUT_FILENO, buf, nrd);
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
+		return (-1);
 
 	close(fd);
 
-	free(buf);
-
-	return (nwr);
+	return (1);
 }
